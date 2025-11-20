0387184d76cc39585a568a094d182a079a5db167
abhiram2504 <abhiram.rustagi@gmail.com>
2025-11-19 20:19:03 -0500

    blending lmao

---
+++ Assets/BlenderZone.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BlenderZone : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioSource audioSource;       // Drag an AudioSource here
+    public AudioClip blendSound;          // Drag your blender sound here
+
+    [Header("Settings")]
+    public string ingredientTag = "Ingredient";  // Tag for objects that should disappear
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if this object is an ingredient
+        if (other.CompareTag(ingredientTag))
+        {
+            // Play the blend sound
+            if (audioSource != null && blendSound != null)
+                audioSource.PlayOneShot(blendSound);
+
+            // Destroy the ingredient
+            Destroy(other.gameObject);
+        }
+    }
